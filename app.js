626375e4a0ea2fb3df48d365f17d63982b0bacc0
Tiffany Fust <tiffanyfust@gmail.com>
2021-08-13 17:12:06 -0700

    added custom error message and styling to link input, added media queries

--- app.js
+++ app.js
@@ -68,30 +68,28 @@ app.displayLinks = (link) => {
 
 app.init = () => {
     const formEl = document.querySelector('.form');
+    const inputEl = document.querySelector('#linkInput');
+    const errorMsg = document.querySelector('.error');
 
-    formEl.addEventListener('submit', function(e) {
+    formEl.addEventListener('submit', (e) => {
         e.preventDefault();
-        const inputEl = document.querySelector('#linkInput');
         const inputValue = inputEl.value;
 
-        app.getShortLinks(inputValue);
-        inputEl.value = '';
-
-        // const copyButtons = document.querySelectorAll('.copyButton');
-
-        // copyButtons.forEach(button => {
-        //     button.addEventListener('click', (e) => {
-        //         e.preventDefault();
-        //         button.style.background = 'hsl(257, 27%, 26%)';
-        //         button.style.padding = '10px 20px';
-        //         button.innerText = 'Copied!';
-                
-        //         const data = button.previousElementSibling.children[1].textContent;
-        //         navigator.clipboard.writeText(data);
-        //     })
-        // })
+        if(inputValue !== '') {
+            app.getShortLinks(inputValue);
+            inputEl.classList.remove('invalid');
+            errorMsg.style.display = 'none';
+            inputEl.value = '';
+        } else {
+            inputEl.classList.add('invalid');
+            errorMsg.style.display = 'block';
+        }
     })
-    
+
+    inputEl.addEventListener('focus', () => {
+        inputEl.classList.remove('invalid');
+        errorMsg.style.display = 'none';
+    })
 }
 
 app.init();
